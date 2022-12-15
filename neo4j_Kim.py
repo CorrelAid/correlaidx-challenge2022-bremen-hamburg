39fe320af3766269f4debe4234fbb93abcf5c6c1
elauckne <e.lauckner08@gmail.com>
2022-12-15 11:03:17 +0000

    Adjusted neo4j script to print outputs

--- neo4j_Kim.py
+++ neo4j_Kim.py
@@ -1,44 +1,37 @@
-import pandas as pd
 from neo4j import GraphDatabase, basic_auth
 
 driver = GraphDatabase.driver(
   "neo4j://44.202.12.248:7687",
   auth=basic_auth("neo4j", "lead-benefits-asterisks"))
 
-def name_query(name, output_name):
-  cypher_query = '''
-  MATCH (a:Officer {name:$name})-[r:officer_of|intermediary_of|registered_address*..10]-(b)
-  RETURN b.name as name, b.node_id as node_id LIMIT 20
-  '''
-  with driver.session(database="neo4j") as session:
-    results = session.execute_read(lambda tx: tx.run(cypher_query, name=name).data())
-    output_name = pd.DataFrame().from_records(results)
-    driver.close()
-  return output_name
 
-def city_query(city, output_name):
-  cypher_query = f'''
+def name_query(name):
+    cypher_query = '''
+    MATCH (a:Officer {name:$name})-[r:officer_of|intermediary_of|registered_address*..10]-(b)
+    RETURN b.name as name, b.node_id as node_id LIMIT 20
+    '''
+    with driver.session(database="neo4j") as session:
+        results = session.execute_read(lambda tx: tx.run(cypher_query, name=name).data())
+        print(f'{name}: {len(results)} results')
+        driver.close()
+
+
+def city_query(city):
+    cypher_query = f'''
     MATCH (b)
-    WHERE toLower(b.name) CONTAINS {city}
+    WHERE toLower(b.name) CONTAINS toLower(" {city} ")
     RETURN  *
     '''
-  with driver.session(database="neo4j") as session:
-    results = session.execute_read(lambda tx: tx.run(cypher_query).data())
-    output_name = results[0]
-    driver.close()
-  return output_name
+    with driver.session(database="neo4j") as session:
+        results = session.execute_read(lambda tx: tx.run(cypher_query).data())
+        print(f'{city}: {len(results)} results')
+        driver.close()
+
 
 query_list_names = [] #hier Liste an Namensanfragen einfügen
-
-for i in query_list_names:
-  name = str(i)
-  output_name = str(i)+'_df'
-  name_query(name, output_name)
-
+for name in query_list_names:
+    name_query(name)
 
 query_list_cities = [] #hier Liste an Stadtanfragen einfügen
-
-for i in query_list_cities:
-  city = str(i)
-  output_name = str(i)+'_df'
-  city_query(city, output_name)
+for city in query_list_cities:
+    city_query(city)
